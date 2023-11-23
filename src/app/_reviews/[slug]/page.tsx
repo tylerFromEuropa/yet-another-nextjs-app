e0087154ee5554b457e5a0fb71902c7de446d943
Tyler <m0bley@icloud.com>
2023-11-23 09:53:11 +0000

    Initial bugs

---
+++ src/app/_reviews/[slug]/page.tsx
@@ -0,0 +1,10 @@
+export default function page() {
+    return (
+        <main>
+            <article>
+                <h2>This is a review of...</h2>
+                <p>Content</p>
+            </article>
+        </main>
+    );
+}
