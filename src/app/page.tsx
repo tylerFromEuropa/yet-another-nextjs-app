038a9cb2b2998287ed215c6d333b8a47453e60fe
Tyler <m0bley@icloud.com>
2023-11-23 10:01:07 +0000

    Hopefully a fix for apostrophe

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
             <h1>Tyler Reviews... </h1>
             <h2>Your one stop shop for reviews. Look no further.</h2>
-            <p>Whether it's video games, movies or TV shows, you've come to the right place to read a review!</p>
+            <p>Whether it&apos;s video games, movies or TV shows, you&apos;ve come to the right place to read a review!</p>
         </main>
     );
 }
